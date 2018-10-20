db4b516d9640ff0e2080579784e41a8ffd096b0c
ashish kumar singh <ashishkumarsinghh@outlook.com>
2018-10-20 23:23:04 +0530

    created get/put/post/delete request routes api endpoints

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,41 @@ const routes = [
     handler: (req, h) => {
       return "Hello From Hapi !";
     }
+  },
+  {
+    method: "GET",
+    path: "/api/v1/teams",
+    handler: (req, h) => {
+      return "Here are all your teams.";
+    }
+  },
+  {
+    method: "GET",
+    path: `/api/v1/teams/{tname}`,
+    handler: (req, h) => {
+      return `Details of team ${encodeURIComponent(req.params.tname)}`;
+    }
+  },
+  {
+    method: "POST",
+    path: `/api/v1/teams/`,
+    handler: (req, h) => {
+      return `Cerating a new team.`;
+    }
+  },
+  {
+    method: "PATCH",
+    path: `/api/v1/teams/{tname}`,
+    handler: (req, h) => {
+      return `Update Details of team ${encodeURIComponent(req.params.tname)}`;
+    }
+  },
+  {
+    method: "DELETE",
+    path: `/api/v1/teams/{tname}`,
+    handler: (req, h) => {
+      return `Delete Details of team ${encodeURIComponent(req.params.tname)}`;
+    }
   }
 ];
 
